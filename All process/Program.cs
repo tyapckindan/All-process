91d420505d753e63de6aee9635c57364b2f07ee8
Данила Тяпкин <dtapkin6@gmail.com>
2023-09-11 13:31:39 +0300

    Добавьте файлы проекта.

---
+++ All process/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AllInfoProcess();
+
+        static void AllInfoProcess()
+        {
+            var myProcess = from proc in Process.GetProcesses(".")
+                            orderby proc.Id
+                            select proc;
+            Console.WriteLine("\n*** Текущие процессы ***\n");
+            int flag = 0;
+            foreach (var p in myProcess)
+            {
+                if (flag < 3)
+                    Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
+                flag++;
+            }
+
+            Console.WriteLine("-----------------------------------------------");
+
+            foreach (var p in myProcess)
+                    Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
+        }
+    }
+}
